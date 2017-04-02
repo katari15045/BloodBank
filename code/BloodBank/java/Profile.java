113f496bef5a2a151c76ad5014abb2d010319e46
katari Saketh Ram <katari15045@iiitd.ac.in>
2017-04-03 00:43:55 +0530

    Working Able to update Password

--- code/BloodBank/java/Profile.java
+++ code/BloodBank/java/Profile.java
@@ -105,5 +105,6 @@ public class Profile extends AppCompatActivity
     {
         imageEditName.setOnClickListener( new ProfileNameUpdator(Profile.this) );
         imageEditUsername.setOnClickListener( new ProfileUsernameUpdator(Profile.this) );
+        imageEditPassword.setOnClickListener( new ProfilePasswordUpdator(Profile.this) );
     }
 }
